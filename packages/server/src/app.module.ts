c399c0c2929f6c6c14e8ae760069eaa9a4d78a42
rmagur1203 <rmagur1203@naver.com>
2023-07-20 20:31:23 +0900

    update

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database.module';
 import { AdminJSModule } from './jsadmin.module';
 import { ConfigValidator } from './validators/config';
+import ms from 'ms';
 
 @Module({
   imports: [
@@ -12,6 +14,15 @@ import { ConfigValidator } from './validators/config';
       isGlobal: true,
       envFilePath: [`../../.env`, `.env`],
       validationSchema: ConfigValidator,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        ttl: ms(config.get<string>('CACHE_TTL', '5s')),
+        max: config.get<number>('CACHE_MAX', 100),
+      }),
+      inject: [ConfigService],
     }),
     DatabaseModule,
     AdminJSModule,
